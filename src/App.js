81f947f3f4929c6280ffab09fa968f949b31b94f
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-02-20 13:09:44 +0300

    практика

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './Header';
-import Techno from './Techno';
 
 const App = () => {
   return (
-    <div >
-      <Header />
-      <Techno />
+    <div className='app'>
+      <header className='header'>
+        <img src='https://spng.pngfind.com/pngs/s/685-6854970_react-logo-png-png-download-logo-png-reactjs.png'></img>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+          <img src='https://i.pinimg.com/originals/ba/9b/34/ba9b34302728e6021136b0f7695fa8a8.jpg'></img>
+        </div>
     </div>);
 }
 
