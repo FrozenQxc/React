ae856f2a0198595bfe842544bda78718af05114a
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-03-02 11:29:46 +0300

    Подключение: css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
        <img src="https://w7.pngwing.com/pngs/27/375/png-transparent-round-logo-computer-icons-web-button-internet-web-design-text-logo.png" alt="" />
     </header>;
+
+    
 }
 
 export default Header; 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
+import  s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <div>
             <img src="https://mobimg.b-cdn.net/v3/fetch/94/94c56e15f13f1de4740a76742b0b594f.jpeg" alt="" />
         </div>
-        <div>
+        <div className={s.item}>
             ava + description
         </div>
-        <div>
+        <div className={s.item} >
             my post
         </div>
-        <div>
+        <div className={s.item}>
             New post
         </div>
-        <div>
+        <div className={s.item}>
             post 1
         </div>
-        <div>
+        <div className={s.item}>
             post 2
         </div>
     </div>
+    
+   
 
 }
 export default Profile; 
